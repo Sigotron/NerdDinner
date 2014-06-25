d35c01dd8bc130e56ff1f3ada72e38fa7f410023
Sigotron <sigotron@gmail.com>
2014-06-25 11:30:59 -0700

    Added Code First Database; NerdDinner, RSVP, Dinner

---
+++ NerdDinner/NerdDinner/Dinner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner
+{
+    public class Dinner
+    {
+        public int DinnerID { get; set; }
+        public string Title { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Address { get; set; }
+        public string HostedBy { get; set; }
+
+        // virtual implies lazy loadinf which means that RSVPs 
+        // won't load until the first time it is accessed
+        public virtual ICollection<RSVP> RSVPs { get; set; }
+    }
+}
