9a2ceeb96c84f8c761c65a36cf59dc7343940f34
Sigotron <sigotron@gmail.com>
2014-07-02 13:20:28 -0700

    implemented methods in DinnerRepository

--- NerdDinner/NerdDinner/Models/DinnerRepository.cs
+++ NerdDinner/NerdDinner/Models/DinnerRepository.cs
@@ -1,22 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace NerdDinner.Models
 {
     public class DinnerRepository
     {
+        // field
+        private readonly NerdDinnerEntities _db = new NerdDinnerEntities();
+
         // Query Methods
-        public IQueryable<Dinner> FindAllDinners();
-        public IQueryable<Dinner> FindUpcomingDinners();
-        public Dinner GetDinner(int id);
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return _db.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in _db.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinner GetDinner(int id)
+        {
+            return _db.Dinners.SingleOrDefault(d => d.DinnerId == id);
+        }
 
         // Insert/Delete
-        public void Add(Dinner dinner);
-        public void Delete(Dinner dinner);
+        public void Add(Dinner dinner)
+        {
+            _db.Dinners.Add(dinner);
+        }
+
+
+        public void Delete(Dinner dinner)
+        {
+            if (_db.Dinners.Contains(dinner))
+            {
+                _db.Dinners.Remove(dinner);
+            }
+        }
 
         // Persistence
-        public void Save();
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
