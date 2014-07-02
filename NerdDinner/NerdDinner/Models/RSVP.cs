a7088a287a45c8db86eed388df14669eff733387
Sigotron <sigotron@gmail.com>
2014-07-02 11:45:03 -0700

    added NerdDinnerEntities DbContext class

---
+++ NerdDinner/NerdDinner/Models/RSVP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public class RSVP
+    {
+        public int RsvpId { get; set; }
+
+        public int DinnerId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string AttendeeName { get; set; }
+
+    }
+}
