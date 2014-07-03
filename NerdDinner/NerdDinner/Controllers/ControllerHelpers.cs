772b16806f661bbb75792b0fadfb63d866a04884
Sigotron <sigotron@gmail.com>
2014-07-03 10:35:56 -0700

    added controller helper

---
+++ NerdDinner/NerdDinner/Controllers/ControllerHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ModelBinding;
+using NerdDinner.Models;
+
+namespace NerdDinner.Controllers
+{
+    public class ControllerHelpers
+    {
+        public static void AddRuleViolations(ModelStateDictionary modelState, IEnumerable<RuleViolation> errors)
+        {
+            foreach (RuleViolation issue in errors)
+            {
+                modelState.AddModelError(issue.PropertyName, issue.ErrorMessage);
+            }
+        }
+    }
+
+}
